9aee0abd3bbb4b49ab76ed47475c511c8043c4a7
Kendell Rennie <kenren@udel.edu>
2020-03-04 17:58:16 -0500

    bata

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -6,12 +6,16 @@ import { Scene } from 'phaser';
 
 export default  class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
-  
+  timeleft:Phaser.Time.Clock;
+
+
+
   manObject: ManObject;
   private hero:Hero;
-  guy: Phaser.GameObjects.Image;
+  guy: Phaser.GameObjects.Rectangle;
   player: Phaser.GameObjects.Image;
-  guy2: Phaser.GameObjects.Image;
+  guy2: Phaser.GameObjects.Rectangle;
+  
   cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
   constructor() {
     super({ key: 'MainScene' });
@@ -19,13 +23,27 @@ export default  class MainScene extends Phaser.Scene {
   }
   create() {
    // this.exampleObject = new ExampleObject(this, 0, 0);  
-      this.guy= this.add.image(100,100,'guy');
-      this.guy2= this.add.image(110,100,'guy2');
+     // this.guy= this.add.image(100,100,'guy');
+      //this.guy2= this.add.image(110,100,'guy2');
+      this.timeleft;
+      //this.add.text(100,100,"hello",0xff0);
+      this.guy=this.add.rectangle(100,100,10,10,0xff0000); 
+      this.guy2=this.add.rectangle(100,100,10,10,0xff0000); 
+      
       this.cursorKeys=this.input.keyboard.createCursorKeys();
 
-    this.player=this.add.image(200,200,'player')
+      
+    this.player=this.add.image(200,200,'player');
+    
+    this.physics.add.collider(this.player,this.guy, function(player,guy2){
+      player.destroy();
+    })
+    this.physics.add.collider(this.player,this.guy, function(player,guy1){
+      player.destroy();
+    })
 
-  }
+    }
+  
 
  
   moveHero(hero:Phaser.GameObjects.Image){
@@ -39,12 +57,12 @@ export default  class MainScene extends Phaser.Scene {
     this.player.x-=2;
   }
 
-  resetEnemy(enemy:Phaser.GameObjects.Image){
+  resetEnemy(enemy:Phaser.GameObjects.Rectangle){
     enemy.y=Math.random() *25;
     
     enemy.x=Math.random() *300;
   }
-  moveEnemy(enemy:Phaser.GameObjects.Image,speed:number){
+  moveEnemy(enemy:Phaser.GameObjects.Rectangle,speed:number){
     enemy.y+=speed;
 
     if (enemy.y>400){
@@ -54,9 +72,8 @@ export default  class MainScene extends Phaser.Scene {
 
   
   moveplayer(){
-    if(this.cursorKeys.up?.isDown){
+    if(this.cursorKeys.up?.isDown) {
       this.player.y-=5;
-      
     }
     if(this.cursorKeys.down?.isDown){
       this.player.y+=5;
@@ -67,14 +84,28 @@ export default  class MainScene extends Phaser.Scene {
     if(this.cursorKeys.right?.isDown){
       this.player.x+=5;
     }
+    if ((this.cursorKeys.space?.isDown)&&(this.cursorKeys.up?.isDown ) ){
+      this.player.y-=20;
+    }
+    if ((this.cursorKeys.space?.isDown)&&(this.cursorKeys.down?.isDown ) ){
+      this.player.y+=20;
+    }
+    if ((this.cursorKeys.space?.isDown)&&(this.cursorKeys.left?.isDown ) ){
+      this.player.x-=20;
+    }
+    if ((this.cursorKeys.space?.isDown)&&(this.cursorKeys.right?.isDown ) ){
+      this.player.x+=20;
+    }
   }
+
 
  
 
   update() {
  
- this.moveEnemy(this.guy,10);
- this.moveEnemy(this.guy2,10);
+ this.moveEnemy(this.guy,5);
+ this.moveEnemy(this.guy2,5);
+ //this.moveHero(this.player);
  this.moveplayer();
 
   }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -11,7 +11,7 @@ export default class PreloadScene extends Phaser.Scene {
   preload() {
       this.load.image('guy',"assets/little_man.png");
       this.load.image("guy2","assets/little_man.png");
-      this.load.image("guy3","assets/little_man.png");
+      //this.load.image("guy3","assets/little_man.png");
       this.load.image("player","assets/player.png");
   }
 
