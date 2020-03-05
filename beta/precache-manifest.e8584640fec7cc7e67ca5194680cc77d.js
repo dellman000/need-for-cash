b81215410881ee636ce58d4662fd6a1247ec5364
Kendell Rennie <kenren@udel.edu>
2020-03-05 00:18:36 -0500

    Updates

---
+++ beta/precache-manifest.e8584640fec7cc7e67ca5194680cc77d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-dellman000/betaassets/citations.md"
+  },
+  {
+    "revision": "c5931788157c31d74dc1d3d473c25cb9",
+    "url": "/coding-2-try-phaser-dellman000/betaassets/little_man.png"
+  },
+  {
+    "revision": "7629cf51b6cee6fac69162ced0dea4eb",
+    "url": "/coding-2-try-phaser-dellman000/betaassets/player.png"
+  },
+  {
+    "revision": "92159adad5dd70e14908",
+    "url": "/coding-2-try-phaser-dellman000/betagen.main.a7598b9d69ab2ac4b2df.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-dellman000/betagen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "cba0a7e3a9582149a4ea5976a65e652d",
+    "url": "/coding-2-try-phaser-dellman000/betaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-dellman000/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-dellman000/betasw.js"
+  }
+]);
