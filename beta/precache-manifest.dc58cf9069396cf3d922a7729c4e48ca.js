07f939b5268268299fe4278063097f48f51394de
Kendell Rennie <kenren@udel.edu>
2020-03-05 22:27:13 -0500

    Updates

--- beta/precache-manifest.dc58cf9069396cf3d922a7729c4e48ca.js
+++ beta/precache-manifest.dc58cf9069396cf3d922a7729c4e48ca.js
@@ -8,19 +8,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/coding-2-try-phaser-dellman000/betaassets/little_man.png"
   },
   {
+    "revision": "7e235f26fb523b07ba03c9d733cdc4e6",
+    "url": "/coding-2-try-phaser-dellman000/betaassets/mothership.png"
+  },
+  {
     "revision": "7629cf51b6cee6fac69162ced0dea4eb",
     "url": "/coding-2-try-phaser-dellman000/betaassets/player.png"
   },
   {
-    "revision": "7ab8da4bfdbc09a1f628",
-    "url": "/coding-2-try-phaser-dellman000/betagen.main.73ecc03715b775d2476f.bundle.js"
+    "revision": "d39a47a33b85566527cd",
+    "url": "/coding-2-try-phaser-dellman000/betagen.main.fe08233ffdd24eb7d8c4.bundle.js"
   },
   {
     "revision": "201b65bee9df6270f739",
     "url": "/coding-2-try-phaser-dellman000/betagen.vendors.a047ee0740c7d21714d9.bundle.js"
   },
   {
-    "revision": "1dfe186ccd6f67734b570338893b68a6",
+    "revision": "51c6eed72b2e701aa42c978418e94783",
     "url": "/coding-2-try-phaser-dellman000/betaindex.html"
   },
   {
--- beta/sw.js
+++ beta/sw.js
@@ -1,4 +1,4 @@
-importScripts("/coding-2-try-phaser-dellman000/betaprecache-manifest.3fea9dce41888160c481b57dffb1a549.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-dellman000/betaprecache-manifest.dc58cf9069396cf3d922a7729c4e48ca.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
