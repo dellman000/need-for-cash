275e04c07b14401d8456991e92431840fa4e8432
Kendell Rennie <kenren@udel.edu>
2020-03-06 15:47:06 -0500

    Updates

---
+++ final/precache-manifest.8b9f6e31a0f22984756c7b497231a655.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-dellman000/finalassets/citations.md"
+  },
+  {
+    "revision": "c5931788157c31d74dc1d3d473c25cb9",
+    "url": "/coding-2-try-phaser-dellman000/finalassets/little_man.png"
+  },
+  {
+    "revision": "7e235f26fb523b07ba03c9d733cdc4e6",
+    "url": "/coding-2-try-phaser-dellman000/finalassets/mothership.png"
+  },
+  {
+    "revision": "7629cf51b6cee6fac69162ced0dea4eb",
+    "url": "/coding-2-try-phaser-dellman000/finalassets/player.png"
+  },
+  {
+    "revision": "31e52d2a8c8b576ed09f62ca03ca3ba8",
+    "url": "/coding-2-try-phaser-dellman000/finalassets/space.png"
+  },
+  {
+    "revision": "db3943a15a7059633cf3f775035c017c",
+    "url": "/coding-2-try-phaser-dellman000/finalassets/space2.png"
+  },
+  {
+    "revision": "d84801ee514cfbd500bce4328058840e",
+    "url": "/coding-2-try-phaser-dellman000/finalassets/stars.png"
+  },
+  {
+    "revision": "21bf19e6c486ff4cec56",
+    "url": "/coding-2-try-phaser-dellman000/finalgen.main.b05f9121144f55c1724e.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-dellman000/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "1e807831626aac8aadca6b101c84da28",
+    "url": "/coding-2-try-phaser-dellman000/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-dellman000/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-dellman000/finalsw.js"
+  }
+]);
---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/coding-2-try-phaser-dellman000/finalprecache-manifest.8b9f6e31a0f22984756c7b497231a655.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
