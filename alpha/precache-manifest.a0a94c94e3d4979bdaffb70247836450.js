09a9859f94597bbad8555cb5676766430202f091
Kendell Rennie <kenren@udel.edu>
2020-04-24 18:46:35 -0400

    Updates

---
+++ alpha/precache-manifest.a0a94c94e3d4979bdaffb70247836450.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7f2bf4e2f2b5bd1b70ef6a03d6bc77",
+    "url": "/need-for-cash/alphaassets/.profit_folders.png-autosave.kra"
+  },
+  {
+    "revision": "f7c70bb8b136a8e262054cf986b70722",
+    "url": "/need-for-cash/alphaassets/car_garage_background_draft.png"
+  },
+  {
+    "revision": "0fa142138f857667e7421488b91fbd05",
+    "url": "/need-for-cash/alphaassets/car_garage_background_pully.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/need-for-cash/alphaassets/citations.md"
+  },
+  {
+    "revision": "8790427433aa36074f539f4bd1063d93",
+    "url": "/need-for-cash/alphaassets/folders_small.png"
+  },
+  {
+    "revision": "aa277cb0faa8b4eb64bfebb032794933",
+    "url": "/need-for-cash/alphaassets/loan.png"
+  },
+  {
+    "revision": "662654fd89811553cb763cffb7f4f2e8",
+    "url": "/need-for-cash/alphaassets/nextturnbutton.png"
+  },
+  {
+    "revision": "7a034e1df97cb6abfad20e078d22c6df",
+    "url": "/need-for-cash/alphaassets/nextturnbutton.png~"
+  },
+  {
+    "revision": "a443e48d6a1b561244e38fe6a4a3a447",
+    "url": "/need-for-cash/alphaassets/profit_folders.png"
+  },
+  {
+    "revision": "3ef6055b81cad75ceccdf6dfd60f70da",
+    "url": "/need-for-cash/alphaassets/profit_folders_DNU.png"
+  },
+  {
+    "revision": "b0a775589b7826ad245226f9a997dc56",
+    "url": "/need-for-cash/alphaassets/profit_folders_small.png"
+  },
+  {
+    "revision": "fe7ce107ecb7c4c6fef0",
+    "url": "/need-for-cash/alphagen.main.856bf72b2569c3829f24.bundle.js"
+  },
+  {
+    "revision": "8857aeb5de5dfb8b65cf",
+    "url": "/need-for-cash/alphagen.vendors.a830139833da967256f6.bundle.js"
+  },
+  {
+    "revision": "e0a4915e5b90ac37132bd4a3a86f632f",
+    "url": "/need-for-cash/alphaindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/need-for-cash/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/need-for-cash/alphasw.js"
+  }
+]);
---
+++ alpha/sw.js
@@ -0,0 +1,11 @@
+importScripts("/need-for-cash/alphaprecache-manifest.a0a94c94e3d4979bdaffb70247836450.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
