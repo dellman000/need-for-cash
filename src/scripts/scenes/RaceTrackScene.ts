d2c8bf443e2b4fe630b09e81f7205e51a65dfdc6
Kendell Rennie <kenren@udel.edu>
2020-04-27 14:56:47 -0400

    test push and pull

--- src/scripts/scenes/RaceTrackScene.ts
+++ src/scripts/scenes/RaceTrackScene.ts
@@ -1,8 +1,12 @@
+
+
 export default class Race_Track_Scene extends Phaser.Scene {
+    hello: any;
     
   
     constructor() {
       super({ key: 'RaceTrack' });
+     
     }
   
     create() {
