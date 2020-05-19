0180334e989e206160e2fd14839da930a5fb3e26
Kendell Rennie <kenren@udel.edu>
2020-05-18 21:59:05 -0400

    Updates

---
+++ final/precache-manifest.256af95cb0018613310dd7f9aed32164.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7f2bf4e2f2b5bd1b70ef6a03d6bc77",
+    "url": "/need-for-cash/finalassets/.profit_folders.png-autosave.kra"
+  },
+  {
+    "revision": "8bf7023800e288b7b26c3e38c015e56e",
+    "url": "/need-for-cash/finalassets/Car_levels/car_level1.png"
+  },
+  {
+    "revision": "a13f661c813cc6e6446b203e9f2652bf",
+    "url": "/need-for-cash/finalassets/Car_levels/car_level2.png"
+  },
+  {
+    "revision": "77a83b97fb2034718f59e58e7dc69727",
+    "url": "/need-for-cash/finalassets/Car_levels/car_level3.png"
+  },
+  {
+    "revision": "f0c3c909ced7592b96efd2c2d441ecad",
+    "url": "/need-for-cash/finalassets/Car_levels/car_level4.png"
+  },
+  {
+    "revision": "23eed8db10b9f498905152175dcac112",
+    "url": "/need-for-cash/finalassets/Logos/Capital_One_final.png"
+  },
+  {
+    "revision": "74de007341d1fb1bddf31325ec5a3681",
+    "url": "/need-for-cash/finalassets/Logos/PNC_button_final.png"
+  },
+  {
+    "revision": "2f113efff0ce5c9df52c7d967c94ba81",
+    "url": "/need-for-cash/finalassets/Logos/chase-logo_final.png"
+  },
+  {
+    "revision": "c94e9aa4c89cb6a52cf510fff7a3929b",
+    "url": "/need-for-cash/finalassets/Logos/wells fargo-logo_final.png"
+  },
+  {
+    "revision": "38aecda3d9f57063afe578cfb3dbe56b",
+    "url": "/need-for-cash/finalassets/Next Turn Screen.png"
+  },
+  {
+    "revision": "bbd455528bdc059882581be2af2e2fe2",
+    "url": "/need-for-cash/finalassets/Terms_Conditions_tab.png"
+  },
+  {
+    "revision": "59ded95593d177c598b11cfbcc397558",
+    "url": "/need-for-cash/finalassets/Terms_Conditions_tab_button.png"
+  },
+  {
+    "revision": "d57250d81d40d53ce732555e7b91cc1f",
+    "url": "/need-for-cash/finalassets/Terms_Conditions_tab_button.png~"
+  },
+  {
+    "revision": "43efbf3d87406588dbede2108c5543a0",
+    "url": "/need-for-cash/finalassets/body1.png"
+  },
+  {
+    "revision": "ae1acd8721cd83de15298fe14479f1e9",
+    "url": "/need-for-cash/finalassets/body2.png"
+  },
+  {
+    "revision": "820821012c1d1c1c6d319af6fecc95a3",
+    "url": "/need-for-cash/finalassets/body3.png"
+  },
+  {
+    "revision": "a5e5717a829b41a3c8ed23416aa38796",
+    "url": "/need-for-cash/finalassets/body4.png"
+  },
+  {
+    "revision": "43efbf3d87406588dbede2108c5543a0",
+    "url": "/need-for-cash/finalassets/car-body-2254989-1882173.png"
+  },
+  {
+    "revision": "3c8f0431953a67f45dabbe493403498b",
+    "url": "/need-for-cash/finalassets/car.png"
+  },
+  {
+    "revision": "a13f661c813cc6e6446b203e9f2652bf",
+    "url": "/need-for-cash/finalassets/car_1_small.png"
+  },
+  {
+    "revision": "f7c70bb8b136a8e262054cf986b70722",
+    "url": "/need-for-cash/finalassets/car_garage_background_draft.png"
+  },
+  {
+    "revision": "0fa142138f857667e7421488b91fbd05",
+    "url": "/need-for-cash/finalassets/car_garage_background_pully.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/need-for-cash/finalassets/citations.md"
+  },
+  {
+    "revision": "8d8925a246f9942856b0196a4d1a99b6",
+    "url": "/need-for-cash/finalassets/engine1.png"
+  },
+  {
+    "revision": "642103d0611f099d4964011d78759a82",
+    "url": "/need-for-cash/finalassets/engine1.png~"
+  },
+  {
+    "revision": "8ffade18a986b398502819a9f42042e3",
+    "url": "/need-for-cash/finalassets/engine2.png"
+  },
+  {
+    "revision": "d1b82aa0ed1dc6a4eed485825950614b",
+    "url": "/need-for-cash/finalassets/engine3.png"
+  },
+  {
+    "revision": "3b4d574634a89bcc30da90e6b729be1d",
+    "url": "/need-for-cash/finalassets/engine4.png"
+  },
+  {
+    "revision": "8df58b0047f2c2f5006d49e6fc236c0d",
+    "url": "/need-for-cash/finalassets/engine5.png"
+  },
+  {
+    "revision": "738a1d2043b8a1ffbcdd7d1f1e99c04b",
+    "url": "/need-for-cash/finalassets/folders_small.png"
+  },
+  {
+    "revision": "1a727c0008ef2ba697d0b6693b58c5c7",
+    "url": "/need-for-cash/finalassets/garagebutton.png"
+  },
+  {
+    "revision": "6a7ae62ba60cf23e951e65a323dae584",
+    "url": "/need-for-cash/finalassets/inventory Screen/EquipItem.png"
+  },
+  {
+    "revision": "c549be12c1b1dbfeb495472ea6fc835a",
+    "url": "/need-for-cash/finalassets/inventory Screen/EquipItem.png~"
+  },
+  {
+    "revision": "08eba77217be23aa738d0a68afdef383",
+    "url": "/need-for-cash/finalassets/inventory Screen/Inventory_manager_Screen.png"
+  },
+  {
+    "revision": "8943eb77eab74364bd3780239c6df1d8",
+    "url": "/need-for-cash/finalassets/inventory Screen/Itemframe.png"
+  },
+  {
+    "revision": "921fcf141d97c479fe1140bfbe3f0fad",
+    "url": "/need-for-cash/finalassets/inventory Screen/green_button.png"
+  },
+  {
+    "revision": "ae588c2a4b25757285b219789b80e827",
+    "url": "/need-for-cash/finalassets/inventory Screen/info_panel.png"
+  },
+  {
+    "revision": "c7e449b1a500e554c7d7fa50334236d7",
+    "url": "/need-for-cash/finalassets/inventory Screen/inventorybutton_new.png"
+  },
+  {
+    "revision": "58c1ee5617847da7e469dac5eeef2a0e",
+    "url": "/need-for-cash/finalassets/inventory Screen/red_button.png"
+  },
+  {
+    "revision": "aa277cb0faa8b4eb64bfebb032794933",
+    "url": "/need-for-cash/finalassets/loan.png"
+  },
+  {
+    "revision": "889b667f72b6c5368b61cd2391441e92",
+    "url": "/need-for-cash/finalassets/loan_button.png"
+  },
+  {
+    "revision": "c3c4989226b39efeb0efc0a8917058a3",
+    "url": "/need-for-cash/finalassets/money_window.png"
+  },
+  {
+    "revision": "662654fd89811553cb763cffb7f4f2e8",
+    "url": "/need-for-cash/finalassets/nextturnbutton.png"
+  },
+  {
+    "revision": "7a034e1df97cb6abfad20e078d22c6df",
+    "url": "/need-for-cash/finalassets/nextturnbutton.png~"
+  },
+  {
+    "revision": "881df533ae910cd5669192c60c15d647",
+    "url": "/need-for-cash/finalassets/onle engines/engine1.png"
+  },
+  {
+    "revision": "e12055c0460d4d40457e3c7b2baafee5",
+    "url": "/need-for-cash/finalassets/onle engines/engine2.png"
+  },
+  {
+    "revision": "c7bbd1c4bc15a915eed465f8ce140461",
+    "url": "/need-for-cash/finalassets/onle engines/engine3.png"
+  },
+  {
+    "revision": "c3a37a5340ec653f42bc0d7cb0a477b7",
+    "url": "/need-for-cash/finalassets/onle engines/engine4.png"
+  },
+  {
+    "revision": "2c4d55c5544363a5f7af7d996c561ae2",
+    "url": "/need-for-cash/finalassets/pc_car.png"
+  },
+  {
+    "revision": "309c8240e3f3a5e354a88e693908d514",
+    "url": "/need-for-cash/finalassets/profit_folderbutton.png"
+  },
+  {
+    "revision": "a443e48d6a1b561244e38fe6a4a3a447",
+    "url": "/need-for-cash/finalassets/profit_folders.png"
+  },
+  {
+    "revision": "3ef6055b81cad75ceccdf6dfd60f70da",
+    "url": "/need-for-cash/finalassets/profit_folders_DNU.png"
+  },
+  {
+    "revision": "43a0e4ad746084188f763d9c0738f583",
+    "url": "/need-for-cash/finalassets/profit_folders_small.png"
+  },
+  {
+    "revision": "b0a775589b7826ad245226f9a997dc56",
+    "url": "/need-for-cash/finalassets/profit_folders_small.png~"
+  },
+  {
+    "revision": "e3af189fb3f24351ceffd79d4a76f349",
+    "url": "/need-for-cash/finalassets/race_button.png"
+  },
+  {
+    "revision": "78642b7b97beea665d92423d4c362c1a",
+    "url": "/need-for-cash/finalassets/racetrack.png"
+  },
+  {
+    "revision": "41602e1296dd0d068056ae6c73b941a8",
+    "url": "/need-for-cash/finalassets/racetrackbutton.png"
+  },
+  {
+    "revision": "7240e4f05696551a47cf467cd77c37f2",
+    "url": "/need-for-cash/finalassets/tire1.png"
+  },
+  {
+    "revision": "cf1415459761f78e36c442ebd7e4f59d",
+    "url": "/need-for-cash/finalassets/tire2.png"
+  },
+  {
+    "revision": "ec576dd6ddd3f05591f48d3b3d9ecb3d",
+    "url": "/need-for-cash/finalassets/tire3.png"
+  },
+  {
+    "revision": "9f16ef7c0c588016afa402a292b9d816",
+    "url": "/need-for-cash/finalassets/tire4.png"
+  },
+  {
+    "revision": "ac0858941db62c102c7b7571d749cd1e",
+    "url": "/need-for-cash/finalassets/turbo1.png"
+  },
+  {
+    "revision": "2a4fca441185e92258e13ae98f2373cc",
+    "url": "/need-for-cash/finalassets/turbo2.png"
+  },
+  {
+    "revision": "90fd6327c6da7ed52893477f01e7ae02",
+    "url": "/need-for-cash/finalassets/turbo3.png"
+  },
+  {
+    "revision": "d073ed6259002712877448e5758e1712",
+    "url": "/need-for-cash/finalassets/turbo4.png"
+  },
+  {
+    "revision": "252278575c762c31c3e1",
+    "url": "/need-for-cash/finalgen.main.5ffcdda16522e005d815.bundle.js"
+  },
+  {
+    "revision": "2d47a5438a397a533f7a",
+    "url": "/need-for-cash/finalgen.vendors.ea35751ea64448dc7941.bundle.js"
+  },
+  {
+    "revision": "531435e3b1717a60260befa121ce1733",
+    "url": "/need-for-cash/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/need-for-cash/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/need-for-cash/finalsw.js"
+  }
+]);
